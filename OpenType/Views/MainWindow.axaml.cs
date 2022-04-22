314b22fd7b4094cc37f82fd8fef6a650a997a1e0
happys2333 <happys2333@outlook.com>
2022-04-22 16:01:38 +0800

    Initial commit

---
+++ OpenType/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace OpenType.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
